ba1057608c9a3d1c4b1dae79a787632adc3ea645
Kevin Harp <kharp1984@gmail.com>
2022-11-26 18:11:54 -0500

    array methods

--- Project 20/app.js
+++ Project 20/app.js
@@ -1,9 +1,27 @@
-let colors = ['red','orange','yellow'];
+// let colors = ['red','orange','yellow'];
 
-let days = ['Monday','Tuesday','Wednesday'];
+// let days = ['Monday','Tuesday','Wednesday'];
 
-colors[0] = 'Sunday'
+// colors[0] = 'Sunday'
 
-console.log(colors);
+// console.log(colors);
+
+let movieLine = ['Tom','Nancy','Bill','Julie','Nancy'];
+
+// movieLine.push('Pablo');
+// movieLine.push('Oliver');
+// movieLine.push('Eva');
+
+// let person = movieLine.pop()
+
+// let barbell = [];
+
+// barbell.push(45);
+// barbell.push(25);
+// barbell.push(30);
+
+// console.log(barbell);
+
+
 
 
