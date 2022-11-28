b2703e8006aa2961c92ee8ff688a77cee0bbe5f1
Kevin Harp <kharp1984@gmail.com>
2022-11-28 01:23:57 -0500

    basic functions examples

--- Project 24/app.js
+++ Project 24/app.js
@@ -26,3 +26,59 @@ function add(x, y) {
     return sum;
 }
 
+function isShortsWeather(temperature) {
+    let result = '';
+    if (temperature >= 75) {
+        result = true;
+    } else {
+        result = false;
+    }
+    return result;
+}
+
+let arr = [1, 2, 3, 4, 'timmy'];
+
+function lastElement(arr) {
+    if (arr.length === 0) {
+        return null;
+    } else {
+        let lst = arr[arr.length - 1];
+        return lst;
+    }
+}
+
+function capitalize(string) {
+    let firstLetter = string.toUpperCase()[0];
+    let remaningLetters = string.slice(1, string.length);
+    let newString = firstLetter + remaningLetters.toLowerCase();
+    return newString;
+}
+
+let input = [1, 2, 3];
+
+function sumArray(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        let newSum = array[i];
+        console.log(sum);
+        sum += newSum;
+    }
+    return sum;
+}
+
+function returnDay(dayNum) {
+    if (dayNum < 1 || dayNum > 7) {
+        return null;
+    } else {
+        const weekDay = {
+            1: 'Monday',
+            2: 'Tuesday',
+            3: 'Wednesday',
+            4: 'Thursday',
+            5: 'Friday',
+            6: 'Saturday',
+            7: 'Sunday',
+        };
+        return weekDay[dayNum];
+    }
+}
