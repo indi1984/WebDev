2b20a1e5837e852462cd605e5040bf25fb5e754a
Kevin Harp <kharp1984@gmail.com>
2022-11-27 23:18:39 -0500

    adv functions

--- Project 24/app.js
+++ Project 24/app.js
@@ -1,2 +1,19 @@
-let die1 = Math.floor(Math.random() * 6) + 1;
+// let die1 = Math.floor(Math.random() * 6) + 1;
 
+function singSong() {
+    console.log('DOE');
+    console.log('RAE');
+    console.log('MI');
+}
+
+function greet(firstName, lastName) {
+    console.log(`Hey there, ${firstName} ${lastName}.`);
+}
+
+function repeat(str, numTimes) {
+    let result = '';
+    for (let i = 0; i < numTimes; i++) {
+        result += str;
+    }
+    console.log(result);
+}
