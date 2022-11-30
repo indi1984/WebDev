7617b6cefd53ea1130fde2aef063afd667b4f98c
Kevin Harp <kharp1984@gmail.com>
2022-11-29 23:17:13 -0500

    higher functions

---
+++ Project 25/app.js
@@ -0,0 +1,23 @@
+
+// function collectEggs() {
+//     let totalEggs = 6;
+//     console.log(totalEggs);
+// }
+
+// let bird = 'Scarlet Macaw';
+// function birdWatch() {
+//     let bird = 'Great Blue Heron';
+
+// }
+
+// console.log(bird);
+
+
+let radius = 8
+if (radius > 0) {
+    const PI = 3.14159;
+    let msg = 'HIII!';
+}
+
+console.log(radius);
+
