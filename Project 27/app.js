126ca0e9eead10e24b6e08665d4968ce7ef333e6
Kevin Harp <kharp1984@gmail.com>
2022-12-04 13:44:18 -0500

    comments

---
+++ Project 27/app.js
@@ -0,0 +1,21 @@
+//todo ADD TO ONE EXAMPLE PAGE
+
+/* //! setTimeout and setInterval methods:
+
+//* setTimeout (will wait until a specified time passes before calling funciton):
+console.log('HELLOO!...');
+setTimeout(() => {
+	console.log('Are you still there?')
+}, 3000);
+console.log('GOODBYE!!');
+
+//* setInterval (will call function at set time interval:
+const id = setInterval(() => {
+	console.log(Math.random())
+}, 2000);
+
+//* clearInterval (will stop what ever setInterval is running according to its id ("const id" above))
+clearInterval(x);
+*/ //! END
+
+//todo END
