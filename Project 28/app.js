5421f08faccdcce9bdad77def995e51bd04a1933
Kevin Harp <kharp1984@gmail.com>
2022-12-04 17:10:43 -0500

    fin

---
+++ Project 28/app.js
@@ -0,0 +1,15 @@
+
+/*//! Default peramiters:
+
+//* One peramiter:
+	function rollDie(numSides = 6) {
+		return Math.floor(Math.random() * numSides) + 1;
+	};
+
+//* More than one permamiter.  Perams with no default must go first:
+	function greet(person, msg = 'Hey there') {
+		console.log(`${msg}, ${person}`);
+	}
+
+*///! END Default peramiters
+
