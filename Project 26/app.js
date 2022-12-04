985996dc0c743b5ec6b643cce7f57bf4a65db71c
Kevin Harp <kharp1984@gmail.com>
2022-12-04 13:23:49 -0500

    com

--- Project 26/app.js
+++ Project 26/app.js
@@ -1,25 +1,4 @@
-// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-
-// function printNum(element) {
-// 	console.log(element);
-// }
-
-// numbers.forEach(printNum)
-
-
-// numbers.forEach(function (el) {
-// 	if(el % 2 === 0)
-// 	console.log(el);
-// })
-
-// for (let el of numbers) {
-// 	console.log(el);
-// }
-
-// const doubles = numbers.map(function (num) {
-// 	return num * 2;
-// });
-
+//? ARRAY FOR EXAMPLES:
 const movies = [
 	{
 		title: 'Amadeus',
