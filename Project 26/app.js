1fac2b6d3492c5b27b2829b5a021e0b775ee2737
Kevin Harp <kharp1984@gmail.com>
2022-12-04 13:18:45 -0500

    comments

--- Project 26/app.js
+++ Project 26/app.js
@@ -39,31 +39,48 @@ const movies = [
 	},
 ]
 
-//* For Each arrow function:
-// movies.forEach(function (movie) {
-// 	console.log(`${movie.title} - ${movie.score}/100`);
-// });
 
+//todo ADD TO ONE EXAMPLE FILE:
+
+//! .forEach method:
+/*
+//* For Each arrow function:
+movies.forEach(function (movie) {
+	console.log(`${movie.title} - ${movie.score}/100`);
+});
+*/
+
+//! .map method using Arrow Functions:
+/*
 //* (All on one line) Long Form:
-// const newMovies = movies.map(function (movie) { return `${movie.title} - ${movie.score / 10}/10` });
+const newMovies = movies.map(function (movie) { return `${movie.title} - ${movie.score / 10}/10` });
 
 //* (All on one line) Arrow Function when doing more than one thing:
-// const newMovies = movies.map(movie => { return `${movie.title} - ${movie.score / 10}/10` });
+const newMovies = movies.map(movie => { return `${movie.title} - ${movie.score / 10}/10` });
 
 //* (All on one line) Arrow Furnction when doing only one thing:
-// const newMovies = movies.map(movie => `${movie.title} - ${movie.score / 10}/10`);
+const newMovies = movies.map(movie => `${movie.title} - ${movie.score / 10}/10`);
 
 //* (Normal way to write on more than one line) Long Form:
-// const newMovies = movies.map(function (movie) {
-// 	return `${movie.title} - ${movie.score / 10}/10`
-// });
+const newMovies = movies.map(function (movie) {
+	return `${movie.title} - ${movie.score / 10}/10`
+});
 
 //* (Normal way to write on more than one line) Arrow Function when doing more than one thing:
-// const newMovies = movies.map(movie => {
-// 	return `${movie.title} - ${movie.score / 10}/10`
-// });
+const newMovies = movies.map(movie => {
+	return `${movie.title} - ${movie.score / 10}/10`
+});
 
 //* (Normal way to write on more than one line) Arrow Function when doing only one thing:
-// const newMovies = movies.map(movie => (
-// 	`${movie.title} - ${movie.score / 10}/10`
-// ));
+const newMovies = movies.map(movie => (
+	`${movie.title} - ${movie.score / 10}/10`
+));
+
+//* (Normal way to write on more than one line) Arrow Function when doing only one thing:
+const newMovies = movies.map(movie => (
+	${movie.title} - ${movie.score / 10}/10`
+));
+*/
+
+//! Other Functions:
+
