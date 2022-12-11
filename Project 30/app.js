b0d85c5f571af48377da88ed8ace5fcd1ea6eea3
Kevin Harp <kharp1984@gmail.com>
2022-12-11 13:23:39 -0500

    formatting

--- Project 30/app.js
+++ Project 30/app.js
@@ -1,18 +1,16 @@
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
-
-
 const container = document.querySelector('#container');
 
 for (let i = 1; i <= 151; i++) {
 	const pokemon = document.createElement('div');
-	pokemon.classList.add('pokemon');
 	const label = document.createElement('span');
-	label.innerText = `#${i}`;
 	const newImg = document.createElement('img');
+	pokemon.classList.add('pokemon');
+	label.innerText = `#${i}`;
 	newImg.src = `${baseURL}${i}.png`;
 	pokemon.appendChild(newImg);
 	pokemon.appendChild(label);
 	container.appendChild(pokemon);
-}
+};
 
 
