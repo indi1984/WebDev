34c0e30391892de8d586e48395c97263a6a93111
Kevin Harp <kharp1984@gmail.com>
2022-12-17 16:38:52 -0500

    rm log

--- Back End/Project 38/index.js
+++ Back End/Project 38/index.js
@@ -12,7 +12,6 @@ const langCode = franc(input);
 if (langCode === 'und') {
 	console.log('SORRY, We couldn\'t figure it out! Try with more sample text.'.red);
 } else {
-	console.log('');
 	const language = langs.where('3', langCode);
 	console.log(`Our best guess is: ${language.name}.`.green);
 }
