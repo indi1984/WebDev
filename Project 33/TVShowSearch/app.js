7f8d2760ee988c1fcfc2bd40233f03ef1a35539b
Kevin Harp <kharp1984@gmail.com>
2022-12-16 20:52:28 -0500

    1

--- Project 33/TVShowSearch/app.js
+++ Project 33/TVShowSearch/app.js
@@ -2,7 +2,11 @@ const form = document.querySelector('#searchForm');
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
     const searchTerm = form.elements.query.value;
-    const config = { params: { q: searchTerm } }
+    const config = {
+        params: {
+            q: searchTerm
+        }
+    }
     const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
     makeImages(res.data)
     form.elements.query.value = '';
