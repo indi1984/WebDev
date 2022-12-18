ddedcfe05e81cc7c2d3dc40df88c901ecf1c3797
Kevin Harp <kharp1984@gmail.com>
2022-12-17 21:45:55 -0500

    l

--- Back End/Project 39/index.js
+++ Back End/Project 39/index.js
@@ -2,49 +2,49 @@ const express = require('express');
 const app = express();
 
 // app.use((req, res) => {
-// 	console.log('WE GOT A NEW REQUEST!');
-//  //	res.send('HELLO WE GOT YOUR REQ, THIS IS RES!');
-// 	res.send('<h1>This is my webpage!</h1>');
+// console.log('WE GOT A NEW REQUEST!');
+//  //res.send('HELLO WE GOT YOUR REQ, THIS IS RES!');
+// res.send('<h1>This is my webpage!</h1>');
 // });
 
 app.get('/', (req, res) => {
-	res.send('This is the home page.');
+  res.send('This is the home page.');
 });
 
 app.get('/r/:subreddit', (req, res) => {
-	const { subreddit } = req.params;
-	res.send(`<h1>Browsing the ${subreddit} subreddit.</h1>`);
+  const {subreddit} = req.params;
+  res.send(`<h1>Browsing the ${subreddit} subreddit.</h1>`);
 });
 
 app.get('/r/:subreddit/:postID', (req, res) => {
-	const { subreddit, postID } = req.params;
-	res.send(`<h1>Viewing Post ID: ${postID} on the ${subreddit} subreddit.</h1>`);
+  const {subreddit, postID} = req.params;
+  res.send(`<h1>Viewing Post ID: ${postID} on the ${subreddit} subreddit.</h1>`);
 });
 
 app.post('/cats', (req, res) => {
-	res.send('THIS IS A POST REQ TO cats.  THIS IS DIFFERENT THAN A GET REQUEST!');
+  res.send('THIS IS A POST REQ TO cats.  THIS IS DIFFERENT THAN A GET REQUEST!');
 });
 
 app.get('/cats', (req, res) => {
-	res.send('MEOW!');
+  res.send('MEOW!');
 });
 
 app.get('/dogs', (req, res) => {
-	res.send('WOOF!');
+  res.send('WOOF!');
 });
 
 app.get('/search', (req, res) => {
-	const { q } = req.query;
-	if (!q) {
-		res.send('Nothing found if nothing searched.');
-	}
-	res.send(`Search results for: ${q}.`);
+  const {q} = req.query;
+  if (!q) {
+    res.send('Nothing found if nothing searched.');
+  }
+  res.send(`Search results for: ${q}.`);
 });
 
 app.get('*', (req, res) => {
-	res.send('I don\'t know the route to that path.');
+  res.send('I don\'t know the route to that path.');
 });
 
 app.listen(8080, () => {
-	console.log('LISTENING ON PORT 8080!');
+  console.log('LISTENING ON PORT 8080!');
 });
