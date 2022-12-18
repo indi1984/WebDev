80adf4ddaefc8b6ef98cbd740555b936220daaed
Kevin Harp <kharp1984@gmail.com>
2022-12-17 21:47:47 -0500

    1

--- Back End/Project 39/.eslintrc.js
+++ Back End/Project 39/.eslintrc.js
@@ -1,15 +1,31 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es2021': true,
-  },
-  'extends': 'google',
-  'overrides': [
-  ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-  },
-  'rules': {
-  },
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es2021': true
+    },
+    'extends': 'eslint:recommended',
+    'overrides': [
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
 };
