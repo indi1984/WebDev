e0c0a99051be1dff4f98b586addf939ce3370e0f
Kevin Harp <kharp1984@gmail.com>
2022-12-17 22:13:50 -0500

    1

--- Back End/Project 40/index.js
+++ Back End/Project 40/index.js
@@ -5,7 +5,8 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
-app.get('/', (req, res) => {res.render('home');
+app.get('/', (req, res) => {
+    res.render('home');
 });
 
 app.listen(3000, () => {
