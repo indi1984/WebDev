45995860f13185c4e860eaca9af97c97c7f5954f
Kevin Harp <kharp1984@gmail.com>
2022-12-18 15:41:57 -0500

    1

--- Back End/Project 40/index.js
+++ Back End/Project 40/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const path = require('path');
 
@@ -6,13 +7,14 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
-    res.render('home');
+  res.render('home');
 });
 
 app.get('/rand', (req, res) => {
-    res.render('random');
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render('random', {rand: num});
 });
 
 app.listen(3000, () => {
-    console.log('LISTENING ON PORT 3000');
+  console.log('LISTENING ON PORT 3000');
 });
