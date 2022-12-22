2a1d48ea5299bd6d99ab18bab255fa8b5b40a8f6
Kevin Harp <kharp1984@gmail.com>
2022-12-22 14:53:25 -0500

    redirect

--- Back End/Project 42/index.js
+++ Back End/Project 42/index.js
@@ -31,14 +31,25 @@ app.get('/comments', (req, res) => {
   res.render('comments/index', {comments});
 });
 
-app.get('/tacos', (req, res) => {
-  res.send('GET /tacos response');
+app.get('/comments/new', (req, res) => {
+  res.render('comments/new');
 });
 
-app.post('/tacos', (req, res) => {
-  const {meat, qty} = req.body;
-  res.send(`OK, here are your ${qty} ${meat} tacos.`);
+app.post('/comments', (req, res) => {
+  const {username, comment} = req.body;
+  comments.push({username, comment});
+  res.redirect('/comments');
 });
+
+
+// app.get('/tacos', (req, res) => {
+//   res.send('GET /tacos response');
+// });
+
+// app.post('/tacos', (req, res) => {
+//   const {meat, qty} = req.body;
+//   res.send(`OK, here are your ${qty} ${meat} tacos.`);
+// });
 
 app.listen(3000, () => {
   console.log('ON PORT 3000!');
