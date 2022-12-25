622123c7c097b9a116a8468e221e374e4966e361
Kevin Harp <kharp1984@gmail.com>
2022-12-25 18:45:34 -0500

    1

--- Back End/Project 40/index.js
+++ Back End/Project 40/index.js
@@ -9,31 +9,31 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
-  res.render('home');
+    res.render('home');
 });
 
 app.get('/cats', (req, res) => {
-  const cats = [
-    'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston',
-  ];
-  res.render('cats', {cats});
+    const cats = [
+        'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston',
+    ];
+    res.render('cats', {cats});
 });
 
 app.get('/r/:subreddit', (req, res) => {
-  const {subreddit} = req.params;
-  const data = redditData [subreddit];
-  if (data) {
-    res.render('subreddit', {...data});
-  } else {
-    res.render('notfound', {subreddit});
-  }
+    const {subreddit} = req.params;
+    const data = redditData [subreddit];
+    if (data) {
+        res.render('subreddit', {...data});
+    } else {
+        res.render('notfound', {subreddit});
+    }
 });
 
 app.get('/rand', (req, res) => {
-  const num = Math.floor(Math.random() * 10) + 1;
-  res.render('random', {num});
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random', {num});
 });
 
 app.listen(3000, () => {
-  console.log('LISTENING ON PORT 3000');
+    console.log('LISTENING ON PORT 3000');
 });
