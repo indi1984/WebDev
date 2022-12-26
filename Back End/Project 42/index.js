db7a857bd43e3ab8009e524b5dd631c6d74b9bdf
Kevin Harp <kharp1984@gmail.com>
2022-12-26 15:13:53 -0500

    uuid

--- Back End/Project 42/index.js
+++ Back End/Project 42/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const {v4: uuid} = require('uuid');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -10,18 +11,22 @@ app.set('view engine', 'ejs');
 // ? Fake database array:
 const comments = [
   {
+    id: uuid(),
     username: 'Todd',
     comment: 'lol that is so funny!',
   },
   {
+    id: uuid(),
     username: 'Skyler',
     comment: 'I like to go birdwatching with my dog',
   },
   {
+    id: uuid(),
     username: 'Sk8terBoi',
     comment: 'Plz delete your account, Todd',
   },
   {
+    id: uuid(),
     username: 'onlysayswoof',
     comment: 'woof woof woof',
   },
@@ -37,8 +42,14 @@ app.get('/comments/new', (req, res) => {
 
 app.post('/comments', (req, res) => {
   const {username, comment} = req.body;
-  comments.push({username, comment});
+  comments.push({username, comment, id: uuid()});
   res.redirect('/comments');
+});
+
+app.get('/comments/:id', (req, res) => {
+  const {id} = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render('comments/show', {comment});
 });
 
 
