91bb399ebbd826169b468607daf8aedf14169d35
Kevin Harp <kharp1984@gmail.com>
2022-12-27 18:33:48 -0500

    moverride

--- Back End/Project 44/index.js
+++ Back End/Project 44/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-
 const Product = require('./models/product');
+const methodOverride = require('method-override');
+
 
 mongoose.connect('mongodb://172.24.224.10:27017/farmStand', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
@@ -16,10 +17,23 @@ mongoose.connect('mongodb://172.24.224.10:27017/farmStand', {useNewUrlParser: tr
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 app.get('/products', async (req, res) => {
   const products = await Product.find({});
   res.render('products/index', {products});
+});
+
+app.get('/products/new', (req, res) => {
+  res.render('products/new');
+});
+
+app.post('/products', async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  console.log(newProduct);
+  res.redirect(`/products/${newProduct._id}`);
 });
 
 app.get('/products/:id', async (req, res) => {
@@ -27,6 +41,16 @@ app.get('/products/:id', async (req, res) => {
   const product = await Product.findById(id);
   console.log(product);
   res.render('products/show', {product});
+});
+
+app.get('/products/:id/edit', async (req, res) => {
+  const {id} = req.params;
+  const product = await Product.findById(id);
+  res.render('products/edit', {product});
+});
+
+app.put('/products/:id', async (req, res) => {
+
 });
 
 app.listen(3000, () => {
