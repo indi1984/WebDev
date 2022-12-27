e61cbd552e1f13f79f3435d78662d3bee8a15a80
Kevin Harp <kharp1984@gmail.com>
2022-12-27 15:26:48 -0500

    static and instance models

--- Back End/Project 43/product.js
+++ Back End/Project 43/product.js
@@ -34,19 +34,67 @@ const productSchema = new mongoose.Schema({
       default: 0,
     },
   },
+  size: {
+    type: String,
+    enum: ['S', 'M', 'L'],
+  },
 });
 
+// productSchema.methods.greet = function() {
+//   console.log('HELLOW HI HOWDY!');
+//   console.log(`- from ${this.name}`);
+// };
+
+productSchema.methods.toggleOnSale = function() {
+  this.onSale = !this.onSale;
+  return this.save();
+};
+
+productSchema.methods.addCategory = function(newCat) {
+  this.categories.push(newCat);
+  return this.save();
+};
+
+productSchema.statics.fireSale = function() {
+  return this.updateMany({}, {onSale: true, price: 0});
+};
 
 const Product = mongoose.model('Product', productSchema);
 
-const bike = new Product({name: 'Bike Helmet', price: 19.50, categories: ['Cycling', 'Safety']});
-bike.save()
-    .then((data) => {
-      console.log('IT WORKED!');
-      console.log(data);
-    })
-    .catch((err) => {
-      console.log('OH NO ERROR');
-      console.log(err);
-    });
+// const findProduct = async () => {
+//   const foundProduct = await Product.findOne({name: 'Bike Helmet'});
+//   console.log(foundProduct);
+//   await foundProduct.toggleOnSale();
+//   console.log(foundProduct);
+//   await foundProduct.addCategory('Outdoors');
+//   console.log(foundProduct);
+// };
 
+// findProduct();
+
+Product.fireSale().then((res) => console.log(res));
+
+// ? Add New:
+
+// const bike = new Product({name: 'Cycling Jersey', price: 29.50, categories: ['Cycling'], size: 'XS'});
+// bike.save()
+//     .then((data) => {
+//       console.log('IT WORKED!');
+//       console.log(data);
+//     })
+//     .catch((err) => {
+//       console.log('OH NO ERROR');
+//       console.log(err);
+//     });
+
+// ? Update Existing:
+
+// Product.findOneAndUpdate({name: 'Tire Pump'}, {price: -19.99}, {new: true, runValidators: true})
+//     .then((data) => {
+//       console.log('IT WORKED!');
+//       console.log(data);
+//     })
+//     .catch((err) => {
+//       console.log('OH NO ERROR');
+//       console.log(err);
+//     });
