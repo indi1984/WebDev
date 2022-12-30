70759e8d766c870f62f16a3a29cda92c164dee9a
Kevin Harp <kharp1984@gmail.com>
2022-12-30 17:28:53 -0500

    wrapAsync function

--- Back End/Project 44/index.js
+++ Back End/Project 44/index.js
@@ -7,7 +7,6 @@ const AppError = require('./AppError');
 const methodOverride = require('method-override');
 
 
-// ! MONGOOSE CONNECT:
 mongoose.connect('mongodb://172.24.224.10:27017/farmStand2', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
       console.log('MONGO Connection OPEN!!');
@@ -17,7 +16,6 @@ mongoose.connect('mongodb://172.24.224.10:27017/farmStand2', {useNewUrlParser: t
       console.log(err);
     });
 
-// ! APP VARIABLES:
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
@@ -25,91 +23,67 @@ app.use(methodOverride('_method'));
 
 const categories = ['fruit', 'vegetable', 'dairy'];
 
-// ! GET:
+function wrapAsync(fn) {
+  return function(req, res, next) {
+    fn(req, res, next).catch((e) => next(e));
+  };
+};
+
 app.get('/', (req, res) => {
   res.redirect('/products');
 });
 
-app.get('/products', async (req, res, next) => {
-  try {
-    const {category} = req.query;
-    if (category) {
-      const products = await Product.find({category});
-      res.render('products/index', {products, category});
-    } else {
-      const products = await Product.find({});
-      res.render('products/index', {products, category: 'All'});
-    }
-  } catch (e) {
-    next(e);
+app.get('/products', wrapAsync(async (req, res, next) => {
+  const {category} = req.query;
+  if (category) {
+    const products = await Product.find({category});
+    res.render('products/index', {products, category});
+  } else {
+    const products = await Product.find({});
+    res.render('products/index', {products, category: 'All'});
   }
-});
+}));
 
-// ! POST:
-app.post('/products', async (req, res, next) => {
-  try {
-    const newProduct = new Product(req.body);
-    await newProduct.save();
-    res.redirect(`/products/${newProduct._id}`);
-  } catch (e) {
-    next(e);
-  }
-});
+app.post('/products', wrapAsync(async (req, res, next) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  res.redirect(`/products/${newProduct._id}`);
+}));
 
-// ! GET:
 app.get('/products/new', (req, res) => {
   res.render('products/new', {categories});
 });
 
-app.get('/products/:id', async (req, res, next) => {
-  try {
-    const {id} = req.params;
-    const product = await Product.findById(id);
-    if (!product) {
-      throw new AppError('Product Not Found', 404);
-    }
-    res.render('products/show', {product});
-  } catch (e) {
-    next(e);
+app.get('/products/:id', wrapAsync(async (req, res, next) => {
+  const {id} = req.params;
+  const product = await Product.findById(id);
+  if (!product) {
+    throw new AppError('Product Not Found', 404);
   }
-});
+  res.render('products/show', {product});
+}));
 
-// ! PUT:
-app.put('/products/:id', async (req, res, next) => {
-  try {
-    const {id} = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
-    console.log(res.body);
-    res.redirect(`/products/${product._id}`);
-  } catch (e) {
-    next(e);
-  }
-});
+app.put('/products/:id', wrapAsync(async (req, res, next) => {
+  const {id} = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
+  console.log(res.body);
+  res.redirect(`/products/${product._id}`);
+}));
 
-// ! GET:
-app.get('/products/:id/edit', async (req, res, next) => {
-  try {
-    const {id} = req.params;
-    const product = await Product.findById(id);
-    if (!product) {
-      throw new AppError('Product Not Found', 404);
-    }
-    res.render('products/edit', {product, categories});
-  } catch (e) {
-    next(e);
+app.get('/products/:id/edit', wrapAsync(async (req, res, next) => {
+  const {id} = req.params;
+  const product = await Product.findById(id);
+  if (!product) {
+    throw new AppError('Product Not Found', 404);
   }
-});
+  res.render('products/edit', {product, categories});
+}));
 
-// ! DELETE:
-app.delete('/products/:id', async (req, res, next) => {
-  try {
-    const {id} = req.params;
-    await Product.findByIdAndDelete(id);
-    res.redirect('/products');
-  } catch (e) {
-    next(e);
-  }
-});
+app.delete('/products/:id', wrapAsync(async (req, res, next) => {
+  const {id} = req.params;
+  await Product.findByIdAndDelete(id);
+  res.redirect('/products');
+}));
 
 // ! ERROR HANDLER:
 app.use((err, req, res, next) => {
@@ -117,7 +91,6 @@ app.use((err, req, res, next) => {
   res.status(status).send(message);
 });
 
-// ! LISTEN:
 app.listen(3000, () => {
   console.log('APP IS LISTENING ON PORT 3000!');
 });
