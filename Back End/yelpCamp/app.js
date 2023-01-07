50e60171c1c50a34cd4e0e1a12b0f041262488ec
Kevin Harp <kharp1984@gmail.com>
2023-01-07 13:42:00 -0500

    validation reviews

--- Back End/yelpCamp/app.js
+++ Back End/yelpCamp/app.js
@@ -6,7 +6,7 @@ const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
-const {campgroundSchema} = require('./schemas.js');
+const {campgroundSchema, reviewSchema} = require('./schemas.js');
 const Review = require('./models/review');
 const app = express();
 
@@ -32,6 +32,16 @@ app.use(methodOverride('_method'));
 // ! Joi Middleware ************************************************
 const validateCampground = (req, res, next) => {
   const {error} = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+const validateReview = (req, res, next) => {
+  const {error} = reviewSchema.validate(req.body);
   if (error) {
     const msg = error.details.map((el) => el.message).join(',');
     throw new ExpressError(msg, 400);
@@ -83,7 +93,7 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   res.redirect('/campgrounds');
 }));
 
-app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   const review = new Review(req.body.review);
   campground.reviews.push(review);
