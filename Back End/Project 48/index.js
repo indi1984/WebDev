df3c6dd3277a28985bed2054552a718f3bc592d3
Kevin Harp <kharp1984@gmail.com>
2023-01-08 16:47:00 -0500

    intro to auth complete

--- Back End/Project 48/index.js
+++ Back End/Project 48/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const User = require('./models/user');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 const session = require('express-session');
 
 // Mongoose Conneciton Configuration
@@ -39,6 +38,14 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(express.urlencoded({extended: true}));
 
+// * Login Middleware
+const requireLogin = (req, res, next) => {
+  if (!req.session.user_id) {
+    return res.redirect('/login');
+  }
+  next();
+};
+
 // Homepage Route
 app.get('/', (req, res) => {
   res.send('This is the homepage.');
@@ -51,11 +58,7 @@ app.get('/register', (req, res) => {
 
 app.post('/register', async (req, res) => {
   const {username, password} = req.body;
-  const hash = await bcrypt.hash(password, 12);
-  const user = new User({
-    username,
-    password: hash,
-  });
+  const user = new User({username, password});
   await user.save();
   req.session.user_id = user._id;
   res.redirect('/');
@@ -68,10 +71,9 @@ app.get('/login', (req, res) => {
 
 app.post('/login', async (req, res) => {
   const {username, password} = req.body;
-  const user = await User.findOne({username});
-  const validPassword = await bcrypt.compare(password, user.password);
-  if (validPassword) {
-    req.session.user_id = user._id;
+  const foundUser = await User.findAndValidate(username, password);
+  if (foundUser) {
+    req.session.user_id = foundUser._id;
     res.redirect('/secret');
   } else {
     res.redirect('/login');
@@ -80,17 +82,18 @@ app.post('/login', async (req, res) => {
 
 // Logout Route
 app.post('/logout', async (req, res) => {
-  // req.session.user_id = null;
-  req.session.destroy();
+  req.session.user_id = null;
+  // req.session.destroy();
   res.redirect('/login');
 });
 
 // Secret Route
-app.get('/secret', (req, res) => {
-  if (!req.session.user_id) {
-    return res.redirect('/login');
-  }
+app.get('/secret', requireLogin, (req, res) => {
   res.render('secret');
+});
+
+app.get('/topsecret', requireLogin, (req, res) => {
+  res.send('TOP SECRET!');
 });
 
 // Mongoose Console Status
