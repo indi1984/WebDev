37618268dabd4953d31bc5db92a83a91dae186b5
Kevin Harp <kharp1984@gmail.com>
2023-01-08 15:22:54 -0500

    1

---
+++ Back End/Project 48/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const User = require('./models/user');
+
+
+app.get('/secret', (req, res) => {
+  res.send('THIS IS SECRET! YOU CANNOT SEE ME UNLESS YOU ARE LOGGED IN!');
+});
+
+app.listen(3000, () => {
+  console.log('MONGOOSE IS SERVING YOUR APP ON PORT 3000!');
+});
+
