7fcd15372617590bbe38a193af9fe125c2e78762
Kevin Harp <kharp1984@gmail.com>
2023-01-09 17:30:52 -0500

    env dotenv setup for cloud upload api

--- Back End/yelpCamp/app.js
+++ Back End/yelpCamp/app.js
@@ -1,3 +1,8 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+};
+
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -13,6 +18,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const app = express();
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
