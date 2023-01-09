76ace545bc8c584fc17465ec5b4cb2129c779885
Kevin Harp <kharp1984@gmail.com>
2023-01-09 18:30:38 -0500

    images array to mongo

--- Back End/yelpCamp/routes/campgrounds.js
+++ Back End/yelpCamp/routes/campgrounds.js
@@ -10,13 +10,14 @@ const upload = multer({storage});
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    // .post(isLoggedIn,
-    //     validateCampground,
-    //     catchAsync(campgrounds.createCampground));
-    .post(upload.array('image'), (req, res) => {
-      console.log(req.body, req.files);
-      res.send('IT WORKED!');
-    });
+    .post(isLoggedIn,
+        upload.array('image'),
+        validateCampground,
+        catchAsync(campgrounds.createCampground));
+// .post(upload.array('image'), (req, res) => {
+//   console.log(req.body, req.files);
+//   res.send('IT WORKED!');
+// });
 
 router.get('/new',
     isLoggedIn,
