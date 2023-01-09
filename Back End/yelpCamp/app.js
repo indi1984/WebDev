283cda89c35277900c7557b439e9cdf5a94ba99c
Kevin Harp <kharp1984@gmail.com>
2023-01-08 19:51:43 -0500

    User registration added to model and new route.

--- Back End/yelpCamp/app.js
+++ Back End/yelpCamp/app.js
@@ -4,10 +4,14 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+const userRoutes = require('./routes/users');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -26,10 +30,6 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
-
 const sessionConfig = {
   secret: 'thisshouldbeabettersecret',
   resave: false,
@@ -40,17 +40,35 @@ const sessionConfig = {
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 };
+
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// ! FAKE USER ROUTE - TEMPORARY
 app.use((req, res, next) => {
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
   next();
 });
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
+app.use('/', userRoutes);
+
+app.get('/fakeUser', async (req, res) => {
+  const user = new User({email: 'kharp1984@gmail.com', username: 'indi1984'});
+  const newUser = await User.register(user, 'chicken');
+  res.send(newUser);
+});
 
 app.get('/', (req, res) => {
   res.render('home');
