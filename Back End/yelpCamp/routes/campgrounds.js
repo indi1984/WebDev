70437299e4f4d3a4f386630d94bf8c8b20782045
Kevin Harp <kharp1984@gmail.com>
2023-01-09 17:22:35 -0500

    cloudinary setup

--- Back End/yelpCamp/routes/campgrounds.js
+++ Back End/yelpCamp/routes/campgrounds.js
@@ -3,13 +3,19 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const {isLoggedIn, isAuthor, validateCampground} = require('../middleware');
 const campgrounds = require('../controllers/campgrounds');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn,
-        validateCampground,
-        catchAsync(campgrounds.createCampground));
+    // .post(isLoggedIn,
+    //     validateCampground,
+    //     catchAsync(campgrounds.createCampground));
+    .post(upload.array('image'), (req, res) => {
+      console.log(req.body, req.files);
+      res.send('IT WORKED!');
+    });
 
 router.get('/new',
     isLoggedIn,
