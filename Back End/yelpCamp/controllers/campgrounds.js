2e46a5a5c16406fbae97e84c8102899d5c6a8e83
Kevin Harp <kharp1984@gmail.com>
2023-01-09 22:30:26 -0500

    virutal property and thumbnails

--- Back End/yelpCamp/controllers/campgrounds.js
+++ Back End/yelpCamp/controllers/campgrounds.js
@@ -42,7 +42,6 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
   const {id} = req.params;
-  console.log(req.body);
   const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
   const imgs = req.files.map((f) => ({url: f.path, filename: f.filename}));
   campground.images.push(...imgs);
@@ -52,7 +51,6 @@ module.exports.updateCampground = async (req, res) => {
       await cloudinary.uploader.destroy(filename);
     }
     await campground.updateOne({$pull: {images: {filename: {$in: req.body.deleteImages}}}});
-    console.log(campground);
   }
   req.flash('success', 'Successfully updated campground!');
   res.redirect(`/campgrounds/${campground._id}`);
