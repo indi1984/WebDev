f5c3c92d67c85207c2ea6ecdc4920122521eff40
Kevin Harp <kharp1984@gmail.com>
2023-01-10 18:52:51 -0500

    adding cluster map to index page

--- Back End/yelpCamp/public/javascripts/showPageMap.js
+++ Back End/yelpCamp/public/javascripts/showPageMap.js
@@ -8,6 +8,9 @@ const map = new mapboxgl.Map({
   zoom: 10, // starting zoom
 });
 
-new mapboxgl.Marker()
+new mapboxgl.Marker({color: '#e00d1c'})
     .setLngLat(campground.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup({offset: 25}).setHTML(`<div class="mt-3"><h4>${campground.title}</h4></div><p>${campground.location}</p>`))
     .addTo(map);
+
+map.addControl(new mapboxgl.NavigationControl());
