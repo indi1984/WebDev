ad3017cc33b9062493b98d1978522e7cbc2b0c98
Kevin Harp <kharp1984@gmail.com>
2023-01-11 00:52:37 -0500

    atlas db setup

--- Back End/yelpCamp/app.js
+++ Back End/yelpCamp/app.js
@@ -20,7 +20,8 @@ const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const app = express();
 
-
+// const dbUrl = process.env.DB_URL;
+// 'mongodb://localhost:27017/yelp-camp'
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
   useCreateIndex: true,
