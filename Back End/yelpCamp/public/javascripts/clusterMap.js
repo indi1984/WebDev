75dde53cfebcadecb76aeb0a62df9ccb553e68a3
Kevin Harp <kharp1984@gmail.com>
2023-01-10 20:57:13 -0500

    add popups and refactor maps

--- Back End/yelpCamp/public/javascripts/clusterMap.js
+++ Back End/yelpCamp/public/javascripts/clusterMap.js
@@ -4,7 +4,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map',
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-  style: 'mapbox://styles/mapbox/dark-v11',
+  style: 'mapbox://styles/mapbox/light-v11',
   center: [-103.5917, 40.6699],
   zoom: 3,
 });
@@ -32,20 +32,20 @@ map.on('load', () => {
       'circle-color': [
         'step',
         ['get', 'point_count'],
-        '#51bbd6',
-        100,
-        '#f1f075',
-        750,
-        '#f28cb1',
+        '#00BCD4',
+        10,
+        '#2196F3',
+        30,
+        '#3F51B5',
       ],
       'circle-radius': [
         'step',
         ['get', 'point_count'],
+        15,
+        10,
         20,
-        100,
         30,
-        750,
-        40,
+        25,
       ],
     },
   });
@@ -99,10 +99,9 @@ map.on('load', () => {
   // the location of the feature, with
   // description HTML from its properties.
   map.on('click', 'unclustered-point', (e) => {
+    const {popUpMarkup} = e.features[0].properties;
     const coordinates = e.features[0].geometry.coordinates.slice();
-    const mag = e.features[0].properties.mag;
-    const tsunami =
-e.features[0].properties.tsunami === 1 ? 'yes' : 'no';
+
 
     // Ensure that if the map is zoomed out such that
     // multiple copies of the feature are visible, the
@@ -113,10 +112,15 @@ e.features[0].properties.tsunami === 1 ? 'yes' : 'no';
 
     new mapboxgl.Popup()
         .setLngLat(coordinates)
-        .setHTML(
-            `magnitude: ${mag}<br>Was there a tsunami?: ${tsunami}`,
-        )
+        .setHTML(popUpMarkup)
         .addTo(map);
+  });
+
+  map.on('mouseenter', 'unclustered-point', () => {
+    map.getCanvas().style.cursor = 'pointer';
+  });
+  map.on('mouseleave', 'unclustered-point', () => {
+    map.getCanvas().style.cursor = '';
   });
 
   map.on('mouseenter', 'clusters', () => {
